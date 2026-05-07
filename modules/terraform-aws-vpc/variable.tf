80807afd87e8901b954fd951e4e881132c2d2204
sreenivas <sreenivaschitti@gmail.com>
2026-05-07 12:45:43 +0530

    vpc

---
+++ modules/terraform-aws-vpc/variable.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+
+    type = string
+
+    validation {
+      condition = contains(
+        ["dev", "qa", "uat", "prod"], 
+        var.environment
+        )
+      error_message = "Environments should be one of dev, qa, uat or prod"
+    }
+  
+}
+
+variable "vpc_cidr" {
+
+    type = string
+
+    default = "10.0.0.0/16"
+  
+}
+
+variable "vpc_tags" {
+  type    = map
+  default = {}
+}
+
+variable "igw_tags" {
+  type    = map
+  default = {}
+}
