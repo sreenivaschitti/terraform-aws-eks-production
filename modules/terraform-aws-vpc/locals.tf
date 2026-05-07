f353285d1316b146d05edcccc5ee235b68c91e76
sreenivas <sreenivaschitti@gmail.com>
2026-05-07 12:55:09 +0530

    vpc

---
+++ modules/terraform-aws-vpc/locals.tf
@@ -0,0 +1,29 @@
+locals {
+  commontags = {
+
+        Project = var.project
+        Environment = var.environment
+        Terraform = true
+        }
+
+    vpc_finaltags = merge(
+
+            local.commontags,
+            { Name = "${var.project}"-"${var.environment}-vpc"
+            },
+            var.vpc_tags
+
+    )    
+
+
+    igw_finaltags = merge(
+
+            local.commontags,
+            { Name = "${var.project}"-"${var.environment}-igw"
+            },
+            var.igw_tags
+
+    )    
+
+
+}   
