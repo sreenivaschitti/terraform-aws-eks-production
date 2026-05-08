8fccb6fd2b51601d424639712e282b447d1c7dec
sreenivas <sreenivaschitti@gmail.com>
2026-05-08 07:04:07 +0530

    vpc

---
+++ modules/terraform-aws-sg/variable.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+
+    type = string
+
+    validation {
+      condition = contains(
+        ["dev", "qa", "uat", "prod"], 
+        var.environment
+        )
+      error_message = "Environments should be one of dev, qa, uat or prod"
+    }
+  
+}
+
+variable "sg_name" {
+
+    type = string
+  
+}
