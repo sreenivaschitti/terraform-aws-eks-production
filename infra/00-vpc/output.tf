cc7acdf5340dd0a4b5f19213b52543788ad1f22b
sreenivas <sreenivaschitti@gmail.com>
2026-05-08 10:47:23 +0530

    vpc

--- infra/00-vpc/output.tf
+++ infra/00-vpc/output.tf
@@ -1,21 +1,21 @@
-output "az_info" {
+# output "az_info" {
 
-    value = module.vpc.az_info
+#     value = module.vpc.az_info
   
-}
-
-# output "vpc_id" {
-#   #takes the value of vpc_id from the module and makes it available as an output
-#   value = module.vpc.vpc_id
-# }
-# output "public_subnet_ids" {
-#   value = module.vpc.public_subnet_ids
 # }
 
-# output "private_subnet_ids" {
-#   value = module.vpc.private_subnet_ids
-# }
+# # output "vpc_id" {
+# #   #takes the value of vpc_id from the module and makes it available as an output
+# #   value = module.vpc.vpc_id
+# # }
+# # output "public_subnet_ids" {
+# #   value = module.vpc.public_subnet_ids
+# # }
 
-# output "database_subnet_ids" {
-#   value = module.vpc.database_subnet_ids
-# }
+# # output "private_subnet_ids" {
+# #   value = module.vpc.private_subnet_ids
+# # }
+
+# # output "database_subnet_ids" {
+# #   value = module.vpc.database_subnet_ids
+# # }
